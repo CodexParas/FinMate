293ca4659a0de8c956df13002826a68764b90dcc
Paras Gupta <37538941+CodexParas@users.noreply.github.com>
2024-10-05 13:19:53 +0530

    Merged CreateDataController

--- src/main/java/com/paras/FinMate/controllers/CreateDataController.java
+++ src/main/java/com/paras/FinMate/controllers/CreateDataController.java
@@ -2,23 +2,35 @@ package com.paras.FinMate.controllers;
 
 import com.paras.FinMate.common.Response;
 import com.paras.FinMate.requests.CreateCustomerRequest;
+import com.paras.FinMate.requests.TransactionRequest;
 import com.paras.FinMate.services.CustomerService;
+import com.paras.FinMate.services.TransactionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class CustomerController {
+@RequestMapping("/create")
+public class CreateDataController {
+
     private final CustomerService customerService;
+    private final TransactionService transactionService;
 
     @PostMapping("/customer")
     public ResponseEntity<Response> createCustomer (@RequestBody @Valid CreateCustomerRequest createCustomerRequest) {
+        // Customer creation logic
         return ResponseEntity.ok(customerService.createCustomer(createCustomerRequest));
+    }
 
+    @PostMapping("/transaction")
+    public ResponseEntity<Response> transaction (@RequestBody @Valid TransactionRequest transactionRequest) {
+        // Transaction logic
+        return ResponseEntity.ok(transactionService.transaction(transactionRequest));
     }
 
 }
