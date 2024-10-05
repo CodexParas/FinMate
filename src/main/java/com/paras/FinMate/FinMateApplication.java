ca91ebadcb9ec9b9e021e63cb78a47648d77a16d
Paras Gupta <37538941+CodexParas@users.noreply.github.com>
2024-10-05 09:42:23 +0530

    Initial commit

---
+++ src/main/java/com/paras/FinMate/FinMateApplication.java
@@ -0,0 +1,13 @@
+package com.paras.FinMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paras/FinMate/FinMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paras.FinMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
